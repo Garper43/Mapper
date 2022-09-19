08592b08deb6799e6696cda2b58a2fa1046af296
Garper43 <65208305+Garper43@users.noreply.github.com>
2022-09-18 18:57:28 -0700

    ,,,

--- js/tools/pointer.js
+++ js/tools/pointer.js
@@ -23,19 +23,19 @@ let pointer = {
     mouseMoveEvent(ev) {
         if(!tool.active) {return}
 
-        cX = ev.clientX;
-        cY = ev.clientY;
+        let x = (ev.clientX - map.x) / (map.scale * map.image.baseWidth);
+        let y = (ev.clientY - map.y) / (map.scale * map.image.baseHeight);
 
         if(this.selectedType == "point") {
             b = this.selectedPoint.brushIndex;
             s = this.selectedPoint.strokeIndex;
             p = this.selectedPoint.pointIndex;
 
-            map.toolData.brushes[b].points.x[s][p] = cX - map.x;
-            map.toolData.brushes[b].points.y[s][p] = cY - map.y;
+            map.toolData.brushes[b].points.x[s][p] = x;
+            map.toolData.brushes[b].points.y[s][p] = y;
         }else {
-            map.toolData.waypoints[this.selectedWaypointIndex].x = cX - map.x;
-            map.toolData.waypoints[this.selectedWaypointIndex].y = cY - map.y;
+            map.toolData.waypoints[this.selectedWaypointIndex].x = x;
+            map.toolData.waypoints[this.selectedWaypointIndex].y = y;
         }
 
         update();
@@ -53,14 +53,15 @@ let pointer = {
         let cX = ev.clientX;
         let cY = ev.clientY;
 
-        for(b = 0; b < map.toolData.brushes.length; b++) { //iterate through brushes
-            brush = map.toolData.brushes[b];
+        //iterate through brushes
+        for(b = 0; b < map.toolData.brushes.length; b++) {
+            let brush = map.toolData.brushes[b];
         
             for(s = 0; s < brush.points.x.length; s++) { //iterate through strokes
 
                 for(p = 0; p < brush.points.x[s].length; p++) { //iterate through points
-                    x = brush.points.x[s][p]+map.x;
-                    y = brush.points.y[s][p]+map.y;
+                    let x = brush.points.x[s][p] * (map.scale * map.image.baseWidth) + map.x;
+                    let y = brush.points.y[s][p] * (map.scale * map.image.baseHeight) + map.y;
 
                     //check if selected point is close enough
                     if(Math.pow(x-cX, 2) + Math.pow(y-cY, 2) < Math.pow(minRadius, 2)) {
@@ -75,9 +76,10 @@ let pointer = {
             } 
         }  
 
+        //iterate through waypoints
         for(i = 0; i < map.toolData.waypoints.length; i++) {
-            x = map.toolData.waypoints[i].x+map.x;
-            y = map.toolData.waypoints[i].y+map.y;
+            let x = map.toolData.waypoints[i].x * (map.scale * map.image.baseWidth) + map.x;
+            let y = map.toolData.waypoints[i].y * (map.scale * map.image.baseHeight) + map.y;
 
             if(Math.pow(x-cX, 2) + Math.pow(y-cY, 2) < Math.pow(minRadius, 2)) {
                 this.selectedWaypointIndex = i;
